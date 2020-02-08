4f105a1633af6b8207948bd2ce9cd5f115be164d
theonlychunsta <lau_tsechun@hotmail.com>
2020-02-08 18:39:42 -0500

    Updates to GET request, regex

--- Httpc.java
+++ Httpc.java
@@ -1,9 +1,14 @@
+
+import java.io.BufferedReader;
 import java.io.Console;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.net.MalformedURLException;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.*;  
 
@@ -12,10 +17,16 @@ import java.util.regex.*;
 public class Httpc {
 	
 	private static Boolean patternCheck = false;
-	private final static String httpMethodGet = "GET";
-	private final static String httpMethodPost = "POST"; 
-	private final static String fileOption = "-f";
-	private final static String dataOption = "-d";
+	private final static String HTTP_METHOD_GET = "GET";
+	private final static String HTTP_METHOD_POST = "POST"; 
+	private final static String FILE_OPTION = "-f";
+	private final static String DATA_OPTION = "-d";
+	private final static String VERBOSE_OPTION = "-v";
+	public final static int DEFAULT_PORT = 80;
+	
+	private static Boolean isVerbose = false;
+	private static Boolean isData = false;
+	private static Boolean isFile = false;
 
 	public static void main(String[] args) {
 		
@@ -42,7 +53,8 @@ public class Httpc {
 				System.exit(0);
 			}
 			
-			Pattern pattern = Pattern.compile("httpc(\\s+(get|post))(\\s+-v)?(\\s+-h\\s.+:.+)?(\\s+-d\\s+'.+')?(\\s+-f\\s+.+)?(\\s+'.+'*)");	
+			
+			Pattern pattern = Pattern.compile("httpc(\\s+(get|post))(\\s+-v)?(\\s+-h\\s.+:[^\\s]+)?(\\s+-d\\s+'.+')?(\\s+-f\\s+[^\\s]+)?(\\s+'((http[s]?:\\/\\/www\\.|http[s]?:\\/\\/|www\\.)?([^\\/]+)(\\/.+)?)'*)");	
 			
 			// Now create matcher object.
 			Matcher m = pattern.matcher(value); 
@@ -56,31 +68,41 @@ public class Httpc {
 				 * Group 4: data -d						m.group(4)
 				 * Group 5: header -h					m.group(5)
 				 * Group 6: file -f						m.group(6)
-				 * Group 7: URL							m.group(7)
+				 * Group 10: Host						m.group(10)
+				 * Group 11: Path						m.group(11)
 				*/
 				
 				String type = m.group(2).toUpperCase();
-				String host = m.group(7).replaceAll("'", "").trim();
+				String host = m.group(10).replaceAll("'", "").trim();
+				String path = "";
+				
+				if(m.group(11) != null)
+				{
+					path = m.group(11).replaceAll("'", "").trim();
+				}
 				
 				//Check if -d
-				Boolean isData = m.group(4) != null? true:false;
+				isData = m.group(4) != null? true:false;
 				//Check if -f
-				Boolean isFile = m.group(6) != null? true: false;
+				isFile = m.group(6) != null? true: false;
+				//Check if -v
+				isVerbose = m.group(3) != null ? true:false;
 				
-				
-				if(type.equals(httpMethodGet) && (isData || isFile)){
+				//Additional check GET method for cURL
+				if(type.equals(HTTP_METHOD_GET) && (isData || isFile)){
 					 System.out.println("The GET request cannot be combined with the -f or -d options.");
 					 patternCheck = false;
 					 continue;
 				}
 				
-				if(type.equals(httpMethodPost) && isData && isFile){
+				//Additional check on POST method for cURL
+				if(type.equals(HTTP_METHOD_POST) && isData && isFile){
 					System.out.println("The POST request cannot be combined with the -f and the -d options.");
 					patternCheck = false;
 					continue;
 				}
 				
-				httpc("/status/418", host, type, null);
+				httpc(path, host, type, null, isData, isFile, isVerbose);
 			}
 			else {
 				 System.out.println("The input was incorrect. Please try again. Enter '0' to exit");
@@ -88,81 +110,114 @@ public class Httpc {
 		}
 	}
 	
-	public static void httpc(String path, String host, String type, String query) {
+	//https://stackoverflow.com/questions/2271800/how-to-read-the-parameters-and-value-from-the-query-string-using-java
+	 public static Map<String, String> getQueryMap(String query)  
+	 {  
+	     String[] params = query.split("&");  
+	     Map<String, String> map = new HashMap<String, String>();  
+	     for (String param : params)  
+	     {  String [] p=param.split("=");
+	         String name = p[0];  
+	       if(p.length>1)  {String value = p[1];  
+	         map.put(name, value);
+	       }  
+	     }  
+	     return map;  
+	 } 
+	 
+	 
+	 public static void httpGetRequest(String host, String path) throws Exception{
+		 
 		try {
-            if(host == null){
-                Socket socket = new Socket("google.com", 80);
-                InputStream inputStream = socket.getInputStream();
-			    OutputStream outputStream = socket.getOutputStream();
+			//Initialize the socket
+			Socket socket = new Socket(host, DEFAULT_PORT);
+			PrintWriter writer = new PrintWriter(socket.getOutputStream());
+	
+			//https://stackoverflow.com/questions/2214308/add-header-in-http-request-in-java
 			
-			    String request = "GET / HTTP/1.0\r\nHost: www.google.com\r\n\r\n";
-			
-                outputStream.write(request.getBytes());
-                outputStream.flush();
-                
-                StringBuilder response = new StringBuilder();
-                
-                int data = inputStream.read();
-                
-                while(data != -1) {
-                    response.append((char) data);
-                    data = inputStream.read();
-                }
-                
-                System.out.println("redirect response: " + response);
-                socket.close();
-
-            }else{
-
-			Socket socket = new Socket(host, 80);
-			InputStream inputStream = socket.getInputStream();
-            OutputStream outputStream = socket.getOutputStream();
-            
-            System.out.println("aver");
-            System.out.println(path);
-            System.out.println(host);
-            System.out.println(query);
-
-            String request = "";
-            if(type.equals("GET")){
-                if(query == null) {
-                    request = "GET " + path + " HTTP/1.0\r\n\r\n";
-                } else {
-                    request = "GET " + path + "?" +query +" HTTP/1.0\r\n\r\n";
-                }
-            }else if(type.equals("POST")){
-                //NOT COMPLETE - must be able to output json from .txt file (?)
-                String body = "key1=value1&key2=value2";
-                request = "POST /post HTTP/1.0\r\n"
-							+ "Content-Type:application/x-www-form-urlencoded\r\n"
-							+ "Content-Length: " + body.length() +"\r\n"
-							+ "\r\n"
-							+ body;
-            }
-			
-
-			//http://postman-echo.com:100/wikypedia/article/1?key=value
-			
-			outputStream.write(request.getBytes());
-			outputStream.flush();
-			
-			StringBuilder response = new StringBuilder();
-			
-			int data = inputStream.read();
-			
-			while(data != -1) {
-				response.append((char) data);
-				data = inputStream.read();
+			//Define the request
+			String request = "";
+			if(path == "" || path == null) {
+				request = "GET / HTTP/1.0\r\nHost: " + host + "\r\n\r\n";
+			}
+			else {
+				request =  "GET " + path + " HTTP/1.0";
 			}
 			
-			System.out.println("request: " + request);
-			System.out.println("response: " + response);
-            socket.close();
-        }
+			
+			 writer.println(request);
+			 writer.println("");
+			 writer.flush();
+			 
+			 BufferedReader bufRead = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				
+			 String outStr;
+			 String response = "";
+			 
+			 while((outStr = bufRead.readLine()) != null){
+			     response += outStr + "\n";
+			 }
+			 
+			 //Format output as needed
+			 formattedOutputResponse(isVerbose, response);
+			
+			 //Close everything
+			 bufRead.close();
+			 writer.close();
+			 socket.close();
+		}
+		
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	 }
+	 
+	 
+	 public static void httpPostRequest () {
+		 
+		 //NOT COMPLETE - must be able to output json from .txt file (?)
+         String body = "key1=value1&key2=value2";
+         String request = "POST /post HTTP/1.0\r\n"
+						+ "Content-Type:application/x-www-form-urlencoded\r\n"
+						+ "Content-Length: " + body.length() +"\r\n"
+						+ "\r\n"
+						+ body;
+	 }
+	
+	
+	 public static void httpc(String path, String host, String type, String query, Boolean isData, Boolean isFile, Boolean isVerbose) {
+		try {
+            if(host == null || host.equals("")){
+               host = "google.com";
+            }
+   
+			//https://stackoverflow.com/questions/2214308/add-header-in-http-request-in-java
+            if(type.equals("GET")){
+            	httpGetRequest(host, path);
+            }
+            else if(type.equals("POST")){
+               httpPostRequest();
+            }
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
     }
+
+	
+	private static void formattedOutputResponse(boolean isVerbose, String response) {
+		
+		if(isVerbose) {
+			System.out.println(response);
+		}
+		
+		else {
+			String[] responseFormatted = response.split("\n\n");
+	        
+			for(int i = 1; i < responseFormatted.length; i++)
+				System.out.println(responseFormatted[i]);
+		}
+	}
 	
 }
